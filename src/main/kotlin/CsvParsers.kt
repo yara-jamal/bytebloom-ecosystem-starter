0fa6acc52dda59adc65a91c5f2041e63dda2cced
Yara <yara421201443@gmail.com>
2025-11-24 18:52:30 +0300

    Refactor(parsers): Consolidate parsing logic and remove redundant files

--- src/main/kotlin/CsvParsers.kt
+++ src/main/kotlin/CsvParsers.kt
@@ -1,7 +1,18 @@
+import model.MenteeRaw
 import model.TeamRaw
+import model.PerformanceRaw
 import java.io.File
-val allFileLines= File("src/main/resources/teams.csv").readLines().drop(1)
 
+val lines_of_mentee = File("src/main/resources/mentees.csv").readLines().drop(1)
+val allFileLines= File("src/main/resources/teams.csv").readLines().drop(1)
+val lines_of_performance = File("src/main/resources/performance.csv").readLines().drop(1)
+
+fun parseMenteeRaw(): List<MenteeRaw> {
+       return  lines_of_mentee.map {
+          val parts_mentee = it.split(",")
+           MenteeRaw(parts_mentee[0].trim(), parts_mentee[1].trim(), parts_mentee[2].trim())
+       }
+            }
 fun parseTeamData(): List<TeamRaw>?{
 
     return allFileLines.map {currentRaw ->
@@ -10,4 +21,12 @@ fun parseTeamData(): List<TeamRaw>?{
         TeamRaw(teamFields[0],teamFields[1],teamFields[2])
     }
      }
+
+fun parsePerformanceRaw(): List<PerformanceRaw> {
+    return lines_of_performance.map {
+      val parts_performance  = it.split(",")
+        PerformanceRaw(parts_performance[0].trim(), parts_performance[1].trim(), parts_performance[2].trim(),parts_performance[3].trim())
+   } 
+}
+
 
