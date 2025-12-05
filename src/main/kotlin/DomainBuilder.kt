4403f32191d80d551510d913e40f62a394996e7f
shoroq <shoroqalbanna.379@gmail.com>
2025-12-05 22:42:02 +0200

    bytebloom--

---
+++ src/main/kotlin/DomainBuilder.kt
@@ -0,0 +1,42 @@
+//val rawTeams = parseTeamData()
+//val rawMentees = parseMenteeRaw()
+//val rawSubmissions = parsePerformanceRaw()
+import model.MenteeRaw
+import model.TeamRaw
+import model.PerformanceRaw
+import domain.Team
+import domain.Mentee
+
+class DomainBuilder {
+
+    fun buildDomainGraph(
+        rawTeams: List<TeamRaw>,
+        rawMentees: List<MenteeRaw>,
+        rawSubmissions: List<PerformanceRaw>
+    ): List<Team> {
+
+        val teamsById = rawTeams.associateBy({ it.teamId}) { teamRaw ->
+            Team(teamRaw.teamName)
+        }
+
+        val menteesById = mutableMapOf<String, Mentee>()
+        for (rawMentee in rawMentees) {
+            val team = teamsById.values.firstOrNull {
+                it.teamName.contains(rawMentee.teamId, ignoreCase = true)
+            }
+
+            val mentee = Mentee(
+                menteeName = rawMentee.name,
+                menteeId = rawMentee.menteeId,
+                team = team
+            )
+            menteesById[rawMentee.menteeId] = mentee
+        }
+
+        for (rawSubmission in rawSubmissions) {
+            val mentee = menteesById[rawSubmission.menteeId]
+        }
+
+        return teamsById.values.toList()
+    }
+}
